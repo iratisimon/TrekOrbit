d9b0bbfc44b45f5afd3bfbeb070a823edd305789
Irati <iratisimonuranga@gmail.com>
2024-05-06 13:34:49 +0200

    Solucionando errores despues del merge con ElbireDev

--- TrekOrbit/src/main/Main.java
+++ TrekOrbit/src/main/Main.java
@@ -6,16 +6,6 @@ import view.LogIn;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-		/*TravelController controlador = new TravelController();
-		BuyTrip bt = new BuyTrip(controlador);
-		bt.setVisible(true);*/
-		
-		//pruebas
-		
-=======
->>>>>>> e66a2ca2f2fbe06243572986279d2592b5599dc0
 		AccessController controladorAcceso = new AccessController();
 		LogIn login = new LogIn(controladorAcceso);
 		login.setVisible(true);
